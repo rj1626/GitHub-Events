c991c2ba8cf175ab17d9b8e9d36424300f4cee0d
Daniel <daniel@datascientest.com>
2025-02-13 11:22:20 +0000

    PEPE8 refactoring

--- test_file.py
+++ test_file.py
@@ -1,21 +1,26 @@
 import pytest
 
+
 def test_calc_addition():
-  # Function test the output of 2+4
-    output = 2+4
+    # Function tests the output of 2 + 4
+    output = 2 + 4
     assert output == 6
 
-def test_calc_substraction():
-  # Function test the output of 2-4
-    output = 2-4
+
+def test_calc_subtraction():
+    # Function tests the output of 2 - 4
+    output = 2 - 4
     assert output == -2
 
+
 def test_calc_multiply():
-  # Function test the output of 2*4
-    output = 2*4
+    # Function tests the output of 2 * 4
+    output = 2 * 4
     assert output == 8
 
+
 def test_coucou():
-  # Function test if the output return 'hello'
-    output='hello'
+    # Function tests if the output returns 'hello'
+    output = 'hello'
     assert output == 'hello'
+
